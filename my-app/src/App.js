a8c148f63bf944eb161f80b0096cb13f31ee18bb
vinayg2509 <vinayg2509@gmail.com>
2026-04-14 16:17:55 +0530

    [Vinay]Created Navbar using bootstrap and added props

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Navbar from "./component/Navbar.js";
+function App() {
+  return (
+    <>
+      <Navbar title="Flipkart" aboutText="About Us"/>
+    </>
+  );
+}
+
+export default App;
