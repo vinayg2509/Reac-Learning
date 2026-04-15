317f10c0e91612207eb6113304354d40a76db897
vinayg2509 <vinayg2509@gmail.com>
2026-04-15 15:54:01 +0530

    [Vinay]Added simple mode toggling logic using props and useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,34 @@
 import Navbar from "./component/Navbar.js";
 import TextForm from "./component/TextForm.js";
-
-
-
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Navbar title="Flipkart" aboutText="About Us"/>
-      <div className='container'>
-        <TextForm heading="Welcome" label="Enter text to perform operation"/>
-      </div>
+const [mode, setMode] = useState("light");
 
+const toggleMode = () => {
+  if (mode === "light") {
+    setMode("dark");
+    document.body.style.backgroundColor="grey"
+  } else {
+    setMode("light");
+    document.body.style.backgroundColor="white"
 
-    </>
-  );
+  }
+};
+
+return (
+  <>
+    <Navbar
+      title="Flipkart"
+      aboutText="About Us"
+      mode={mode}
+      toggleMode={toggleMode}
+    />
+    <div className="container">
+      <TextForm heading="Welcome" label="Enter text to perform operation" />
+    </div>
+  </>
+);
 }
 
 export default App;
