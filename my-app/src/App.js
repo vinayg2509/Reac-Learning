c1c5c2dab560b35fc9c29988e187bb4331f8bbb1
vinayg2509 <vinayg2509@gmail.com>
2026-04-16 14:08:32 +0530

    [Vinay]Added Routes logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,67 +2,66 @@ import Alert from "./component/Alert.js";
 import Navbar from "./component/Navbar.js";
 import TextForm from "./component/TextForm.js";
 import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import About from "./component/About.js";
 import Home from "./component/Home.js";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-const [mode, setMode] = useState("light");
-const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-const showAlert=(message,type)=>{
-  setAlert({
-    msg:message,
-    type:type
-  })
-  setTimeout(()=>{
-    showAlert(null)
-  },3000)
-}
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
 
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
 
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode enabled", "Success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "Success");
+    }
+  };
 
-const toggleMode = () => {
-  if (mode === "light") {
-    setMode("dark");
-    document.body.style.backgroundColor="grey"
-    showAlert("Dark mode enabled","Success")
-  } else {
-    setMode("light");
-  document.body.style.backgroundColor="white"
-    showAlert("Light mode enabled","Success")
+  return (
+    <BrowserRouter>
+      <Navbar
+        title="Text Util"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
 
+      <Alert alert={alert} />
 
-  }
-};
-
-return (
-  <>
-    <Navbar
-      title="Flipkart"
-      aboutText="About Us"
-      mode={mode}
-      toggleMode={toggleMode}
-    />
-
-      <Alert alert={alert}/>
-
-    <div className="container">
-      <TextForm  showAlert={showAlert} heading="Welcome" label="Enter text to perform operation" />
-    </div>
-
-    <div className='container'>
-       <About/>
-    </div>
-
-    <div className='container'>
-       <Home/>
-    </div>
-
-    
-  </>
-);
+      <Routes>
+        <Route path="/home" element={<div className="container"><Home /></div>} />
+        <Route
+          path="/text-form"
+          element={
+            <div className="container">
+              <TextForm
+                showAlert={showAlert}
+                heading="Welcome"
+                label="Enter text to perform operation"
+              />
+            </div>
+          }
+        />
+        <Route path="/about" element={<div className="container"><About /></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- my-app/src/component/Navbar.js
+++ my-app/src/component/Navbar.js
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <Link className="navbar-brand" to="/home">{props.title}</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,11 +21,15 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.aboutText}</a>
+              <Link className="nav-link" to="about">{props.aboutText}</Link>
+            </li>
+
+               <li className="nav-item">
+              <Link className="nav-link" to="text-form">Textform</Link>
             </li>
           </ul>
 
