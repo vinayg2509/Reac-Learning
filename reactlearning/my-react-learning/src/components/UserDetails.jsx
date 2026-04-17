5ef7f1e95ee0db8b313773054aa2a9cdcecc9083
vinayg2509 <vinayg2509@gmail.com>
2026-04-17 15:04:02 +0530

    [Vinay]Practised login for conditional rendering using ternory operator

--- reactlearning/my-react-learning/src/components/UserDetails.jsx
+++ reactlearning/my-react-learning/src/components/UserDetails.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 
 export default function UserDetails({ name, isOnline, hideOffLine }) {
-  if (hideOffLine && !isOnline) return null;
+//   if (hideOffLine && !isOnline) return null;
 
-  if (isOnline) {
-    return (
-      <>
-        <h3>{name}</h3>
-        <small>Is Available</small>
-        <button>Send Message</button>
-      </>
-    );
-  }
+//   if (isOnline) {
+//     return (
+//       <>
+//         <h3>{name}</h3>
+//         <small>Is Available</small>
+//         <button>Send Message</button>
+//       </>
+//     );
+//   }
 
-  return (
-    <>
-      <h3>{name}</h3>
-      <small>Not Available</small>
-      <p>Check Later</p>
-    </>
-  );
+//   return (
+//     <>
+//       <h3>{name}</h3>
+//       <small>Not Available</small>
+//       <p>Check Later</p>
+//     </>
+//   );
+
+// !Using ternary operator
+
+            return(
+
+                <div>
+                    <h3>{name}</h3>
+                    <small>{isOnline ?"Online":"Offline"}</small>
+                    <p>{isOnline?"Is Avaliable for chat":"Not Avaliable"}</p>
+
+                    {isOnline ?(
+                        <button>Send Message</button>
+                    )
+                    :(
+                        <small>Check Later</small>
+                    )}
+                </div>
+            )
 }
