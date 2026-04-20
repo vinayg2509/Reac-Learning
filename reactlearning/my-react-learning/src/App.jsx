3e9f0e677ac02adba3ffcc06c5439478fb56bff9
vinayg2509 <vinayg2509@gmail.com>
2026-04-20 15:06:52 +0530

    [Vinay]Implemented login app using useState state as string

--- reactlearning/my-react-learning/src/App.jsx
+++ reactlearning/my-react-learning/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   return (
   <>
 
+    <Counter/>
+    <Counter/>
 
       <LoginCard/>
  </>
--- reactlearning/my-react-learning/src/LoginCard.jsx
+++ reactlearning/my-react-learning/src/LoginCard.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import { useState } from 'react'
-
-
+import React from "react";
+import { useState } from "react";
 
 export default function LoginCard() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [message, setMessage] = useState(" ");
 
-      const[isLoggedIn,setIsLoggedIn]=useState(false)
+  const handleLogin = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
 
-      const handleLogin=()=>
-      {
-        setIsLoggedIn(!isLoggedIn)
-      }
-
+  const handelMessage = (event) => {
+    setMessage(event.target.value);
+  };
   return (
-    <div>
-      <button onClick={handleLogin}>{isLoggedIn?"Logout":"Login"}</button>
-    </div>
-  )
+    <>
+      <div>
+        <button onClick={handleLogin}>{isLoggedIn ? "Logout" : "Login"}</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={handelMessage}
+          value={message}
+          placeholder="Type a message"
+        />
+      <p>{message}</p>
+
+
+      </div>
+     
+    </>
+  );
 }
